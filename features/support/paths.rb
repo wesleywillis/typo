844d0b1206b2977f1e19994db95dd09b24a2837c
Wesley Willis <haileyw@gmail.com>
2016-03-28 16:58:15 -0700

    EOD commit - still working on category bug fix

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -18,6 +18,9 @@ module NavigationHelpers
     when /^the new article page$/
       '/admin/content/new'
 
+    when /a list of categories/
+      admin_categories_path
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
